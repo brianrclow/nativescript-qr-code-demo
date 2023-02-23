31438ba6bdf09ceb097c92a62ddb0baa007be2f0
brianrclow <brianrclow@gmail.com>
2023-02-22 22:07:05 -0700

    4 files modified

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { LoadEventData } from '@nativescript/core';
-import { BarcodeResult } from '@nativescript/mlkit-barcode-scanning';
-import { MLKitView } from '@nativescript/mlkit-core';
-import { distinctUntilChanged, firstValueFrom, Subject, take } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { ScannerService } from '../scanner/scanner.service';
 
 @Component({
@@ -10,43 +7,19 @@ import { ScannerService } from '../scanner/scanner.service';
   templateUrl: './event.component.html',
   styleUrls: ['event.component.scss'],
 })
-export class EventComponent implements OnInit {
-  $scannedValue = new Subject<string>();
-  camera: MLKitView;
+export class EventComponent {
   private _scannerService = inject(ScannerService);
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.$scannedValue
-      .pipe(distinctUntilChanged(), take(1))
-      .subscribe((value) => {
-        console.log('GOT VALUE');
-        console.log('VAL: ', value);
-      });
-  }
-
-  onDetection(event): void {
-    const result = event.data[0] as BarcodeResult;
-    this.$scannedValue.next(result.displayValue);
-  }
-
-  onLoaded(event: LoadEventData) {
-    this.camera = event.object as unknown as MLKitView;
-    if (!this.camera.hasCameraPermission()) {
-      this.camera.requestCameraPermission();
-    }
-  }
-
   scanNow() {
-    console.log('Scan Now');
     const ref = this._scannerService.open();
 
     firstValueFrom(ref.afterClosed()).then((value) => {
       console.log('value', value);
-      setTimeout(() => {
-        alert(`you scanned ${value}`);
-      }, 500);
+      if (value != null) {
+        setTimeout(() => {
+          alert(`${value}`);
+        }, 500);
+      }
     });
   }
 }
